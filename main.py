c460b3510755b8840a34b02012d6ea398524fd6c
Jeffrey Ke <jeffk@tesu.pc.cs.cmu.edu>
2025-04-04 23:18:31 -0400

    Added comments

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 def main():
     for t in range(args.iter):
-        rlbench = rlbench.init()
-        policy = dp3()
-        vggt = vggt()
+        rlbench = rlbench.init() # <-- important
+        policy = dp3() # <-- important
+        vggt = vggt() # <-- important
         cameras = rlbench.cameras
         images = cameras.capture()
         sensor = vggt.reconstruct(images) if args.use_vggt else rlbench.depth_camera.capture()
