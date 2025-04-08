c6bf04a1663fc2e339495b774930ad910feef9a2
Jeffrey Ke <jeffk@tesu.pc.cs.cmu.edu>
2025-04-08 13:41:16 -0400

    init, before adding assets or ckpts.

---
+++ sourceme.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/.mujoco/mujoco210/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
+export MUJOCO_GL=egl
+
