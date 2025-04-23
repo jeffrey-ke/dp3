237bd05f201d000b0b31c99bb9305bd07dace2db
Rajath Aralikatti <rajathcaralikatti@gmail.com>
2025-04-23 16:19:12 -0400

    Added code to generate custom resolution  images in Adroit

--- 3D-Diffusion-Policy/scripts/gen_demonstration_adroit.sh
+++ 3D-Diffusion-Policy/scripts/gen_demonstration_adroit.sh
@@ -6,7 +6,7 @@ cd third_party/VRL3/src
 
 task=${1}
 
-CUDA_VISIBLE_DEVICES=0 python gen_demonstration_expert.py --env_name $task \
+CUDA_VISIBLE_DEVICES=0 python gen_demonstration_expert_custom_res.py --env_name $task \
                         --num_episodes 10 \
                         --root_dir "../../../3D-Diffusion-Policy/data/" \
                         --expert_ckpt_path "../ckpts/vrl3_${task}.pt" \
