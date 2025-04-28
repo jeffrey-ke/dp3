0f57d95f6c061d8499276932e4af0ad3cfc24be2
Jeffrey <jeffk613@gmail.com>
2025-04-27 21:23:37 -0400

    Added dino encoder.

---
+++ 3D-Diffusion-Policy/3D-Diffusion-Policy/diffusion_policy_3d/model/vision/dino_encoder.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+from diffusion_policy_3d.model.vision.pointnet_extractor import create_mlp
+class DinoEncoder(nn.Module):
+    def __init__(self, observation_space, out_channel):
+        super().__init__()
+        ## state information
+        state_mlp_output_dim = 64                          
+        self.final_dim = out_channel + state_mlp_output_dim
+        self.n_output_channels = self.final_dim
+        self.robot_state_dim = observation_space['agent_pos']
+        self.image_shape = observation_space['image']
+        ## layers
+        self.dino = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+        self.dino_proj = nn.Linear(self.dino.embed_dim, out_channel)
+        self.state_mlp = nn.Sequential(*create_mlp(self.robot_state_dim[0],
+                                                   state_mlp_output_dim,
+                                                   [state_mlp_output_dim],
+                                                   nn.ReLU))
+    def forward(self, obs):
+        # images ought to be of shape B,3,H,W
+        robot_state = obs["agent_pos"]
+        robot_state_features = self.state_mlp(robot_state) #B,64
+
+        if obs["img"].shape[1] != 3:
+            images = obs["img"].permute(0, 3, 1, 2) 
+        else:
+            images = obs["img"]
+
+        with torch.no_grad():
+            self.dino.eval()
+            dino_features = self.dino(images)
+
+        proj_dino_features = self.dino_proj(dino_features) #B,out_channel
+        cated_features = torch.cat([proj_dino_features, robot_state_features], dim=-1)
+        return cated_features
+
+    def output_shape(self):
+        return self.final_dim
